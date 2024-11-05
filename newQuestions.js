01b137586e88cff8278e67b2c927058d7851247e
Muffakir Hamid <muffakir63@gmail.com>
2024-11-05 23:04:30 +0530

    Solved Questions

--- newQuestions.js
+++ newQuestions.js
@@ -451,3 +451,34 @@ function getOnlyEvens(arr) {
 }
 
 console.log(getOnlyEvens([0, 1, 2, 3, 4]));
+
+
+
+
+
+// Convert All Array Items to String
+// Create a function that takes an array of integers and strings. Convert integers to strings and return the new array.
+
+// Examples
+// parseArray([1, 2, "a", "b"]) ➞ ["1", "2", "a", "b"]
+
+// parseArray(["abc", 123, "def", 456]) ➞ ["abc", "123", "def", "456"]
+
+// parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]) ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
+
+// parseArray([]) ➞ []
+
+
+
+function parseArray(arr) {
+    let newArr = []
+    if(Array.isArray(arr)) {
+        for(let i = 0; i < arr.length; i++) {
+            newArr.push(String(arr[i]))
+        }
+    }
+    return newArr
+}
+
+
+console.log(parseArray([1,2,3,'w,s']));
